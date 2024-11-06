97062823e1c4e9abfc458bf577d7e658229d2a0f
Ariel Tyson <ariel30190@hotmail.com>
2024-11-06 13:02:43 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ariel Tyson on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
